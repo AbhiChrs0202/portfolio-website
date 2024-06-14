e021b432c7b19933c4d23a3769fa82ea5b168570
Abhishek Chaurasia <abhichrs0202@gmail.com>
2024-06-14 11:46:43 +0530

    all responsive 1

--- src/components/SocialLinks.jsx
+++ src/components/SocialLinks.jsx
@@ -1,9 +1,11 @@
-import React from "react";
-import { FaGithub, FaLinkedin } from "react-icons/fa";
+import React, { useState } from "react";
+import { FaGithub, FaLinkedin, FaTimes } from "react-icons/fa";
 import { HiOutlineMail } from "react-icons/hi";
 import { BsFillPersonLinesFill } from "react-icons/bs";
 
 const SocialLinks = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
   const links = [
     {
       id: 1,
@@ -47,30 +49,66 @@ const SocialLinks = () => {
   ];
 
   return (
-    <div className="flex flex-col top-[35%] left-0 fixed lg:flex-row lg:top-auto lg:right-[35%] lg:left-auto lg:bottom-0 lg:fixed">
-      <ul className="flex flex-col lg:flex-row lg:space-x-4">
-        {links.map(({ id, child, href, style, download }) => (
-          <li
-            key={id}
-            className={
-              "flex justify-between items-center w-40 h-14 px-4 lg:w-52 lg:h-16 lg:px-6 ml-[-100px] lg:ml-0 lg:mr-[-120px] lg:hover:mr-0 hover:ml-[-10px] lg:hover:mr-0 hover:rounded-md duration-300 bg-gray-500" +
-              " " +
-              style
-            }
-          >
-            <a
-              href={href}
-              className="flex justify-between items-center w-full text-white"
-              download={download}
-              target="_blank"
-              rel="noreferrer"
+    <>
+      {/* Floating button for smaller screens */}
+      <div className="lg:hidden fixed bottom-4 right-4">
+        <button
+          onClick={() => setIsOpen(!isOpen)}
+          className="bg-gray-500 text-white p-3 rounded-full shadow-md"
+        >
+          {isOpen ? <FaTimes size={24} /> : <FaGithub size={24} />}
+        </button>
+      </div>
+
+      {/* Modal for smaller screens */}
+      {isOpen && (
+        <div className="lg:hidden fixed inset-0 bg-black bg-opacity-70 flex justify-center items-center z-50">
+          <div className="bg-gray-800 p-8 rounded-lg">
+            <ul>
+              {links.map(({ id, child, href, download }) => (
+                <li key={id} className="flex justify-center my-4">
+                  <a
+                    href={href}
+                    className="flex justify-between items-center w-full text-white"
+                    download={download}
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    {child}
+                  </a>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
+      )}
+
+      {/* Sidebar for larger screens */}
+      <div className="hidden lg:flex flex-col top-[35%] left-0 fixed">
+        <ul>
+          {links.map(({ id, child, href, style, download }) => (
+            <li
+              key={id}
+              className={
+                "flex justify-between items-center w-40 h-14 px-4 ml-[-100px] hover:ml-[-10px] hover:rounded-md duration-300 bg-gray-500" +
+                " " +
+                style
+              }
             >
-              {child}
-            </a>
-          </li>
-        ))}
-      </ul>
-    </div>
+              <a
+                href={href}
+                className="flex justify-between items-center w-full text-white"
+                download={download}
+                target="_blank"
+                rel="noreferrer"
+              >
+                {child}
+              </a>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </>
   );
 };
 
