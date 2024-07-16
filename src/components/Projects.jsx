d1ba2a2d1c7e690a9eb625b861a4f4fd83a488f4
Abhishek Chaurasia <abhichrs0202@gmail.com>
2024-07-16 18:35:41 +0530

    project updated

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Sareestyle from "../assets/SareeStyle1.png";
 import netflixpic from "../assets/netflix1.png";
 import cashflow from "../assets/cashflow1.png";
+import edufy from "../assets/Edufy.png";
 
 const Projects = () => {
   const projects = [
@@ -32,6 +33,15 @@ const Projects = () => {
       description:
         "Created a web application for finance tracking, offering a personalized user experience and secure authentication.",
     },
+    {
+      id: 4,
+      src: edufy,
+      name: "Utilized: React.js, MUI, Vercel",
+      url: "https://edufy-course.vercel.app/",
+      url2: "https://github.com/AbhiChrs0202/Edufy-course",
+      description:
+        "Developed a dynamic web application for seamless course selling, featuring user-friendly navigation and course management tools."
+    },
   ];
 
   const handleButtonClick = (url) => {
@@ -45,7 +55,7 @@ const Projects = () => {
   return (
     <div
       name="projects"
-      className="bg-gradient-to-b from-gray-800 to-black w-full text-white md:h-screen pt-[7rem]"
+      className="bg-gradient-to-b from-gray-800 to-black w-full text-white pt-[7rem]"
     >
       <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
         <div className="pb-5">
@@ -55,32 +65,34 @@ const Projects = () => {
           <p className="pt-6">Check out my work</p>
         </div>
 
-        <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-8 px-2 sm:px-0">
+        <div className="grid sm:grid-cols-2 lg:grid-cols-3 gap-8 px-2 sm:px-0">
           {projects.map(({ id, name, src, url, url2, description }) => (
             <div
               key={id}
-              className="shadow-md shadow-gray-600 rounded-lg h-auto flex flex-col"
+              className="shadow-md shadow-gray-600 rounded-lg flex flex-col"
             >
               <img
                 src={src}
                 alt=""
-                className="rounded-md duration-200 hover:scale-105 w-full h-48 md:h-60"
+                className="rounded-t-md duration-200 hover:scale-105 w-full h-48 md:h-60"
               />
-              <p className="pt-2 mx-2 text-sm md:text-base">{name}</p>
-              <p className="mx-2 pt-2 text-sm md:text-base">{description}</p>
-              <div className="flex items-center justify-center mt-auto pb-4">
-                <button
-                  onClick={() => handleButtonClick(url)}
-                  className="w-24 px-3 py-2 mt-2 mb-2 border rounded-lg duration-200 hover:scale-105"
-                >
-                  Demo
-                </button>
-                <button
-                  onClick={() => handleClick(url2)}
-                  className="w-24 px-3 py-2 mt-2 mb-2 ml-3 border rounded-lg duration-200 hover:scale-105"
-                >
-                  Code
-                </button>
+              <div className="p-4 flex flex-col flex-grow">
+                <p className="font-medium text-sm md:text-base">{name}</p>
+                <p className="mt-2 text-sm md:text-base">{description}</p>
+                <div className="flex items-center justify-center mt-auto">
+                  <button
+                    onClick={() => handleButtonClick(url)}
+                    className="w-24 px-3 py-2 mt-4 mb-2 border rounded-lg duration-200 hover:scale-105"
+                  >
+                    Demo
+                  </button>
+                  <button
+                    onClick={() => handleClick(url2)}
+                    className="w-24 px-3 py-2 mt-4 mb-2 ml-3 border rounded-lg duration-200 hover:scale-105"
+                  >
+                    Code
+                  </button>
+                </div>
               </div>
             </div>
           ))}
